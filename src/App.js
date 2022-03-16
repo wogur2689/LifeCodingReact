01b6203a4bafff36635d96748b61f137a7aeeee6
jaehyeokGil <81075170+wogur2689@users.noreply.github.com>
2022-03-16 23:36:18 +0900

    생활코딩 event1

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ class App extends Component {
   constructor(props) {
     super(props); //props 초기화
     this.state = {
+      mode:'welcome', //읽기 페이지인지 아닌지를 나타냄.
       subject:{title:'WEB', sub:'world wide web!'},
+      welcome:{title:'Welcome', desc:'Hello, React!!'},
       contents:[
         {id:1, title:'HTML', desc:'HTML is HyperText ...'}, 
         {id:2, title:'CSS', desc:'CSS is for design'},
@@ -16,7 +18,16 @@ class App extends Component {
       ]
     }
   }
-  render() {
+  render() { //props, state가 바뀌면 화면이 다시 그려짐.
+    console.log('App render');
+    var _title, _desc = null;
+    if(this.state.mode === 'welcome') {
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if(this.state.mode === 'read') {
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
     return (
       <div className="App">
         <Subject 
@@ -24,7 +35,7 @@ class App extends Component {
           sub={this.state.subject.sub}>
         </Subject>
         <TOC data={this.state.contents}></TOC>
-        <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
+        <Content title={_title} desc={_desc}></Content>
       </div>
     );
   }
